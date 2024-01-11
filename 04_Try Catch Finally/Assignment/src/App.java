c49f34dd5f128b75f22ff1ef60c6cf18d3a0cd11
Braden Hamilton <hamilbra002@warren.k12.in.us>
2024-01-11 10:40:14 -0500

    FInally lets try to catch

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter another number: ");
+        int secondNumber = input.nextInt();
+        
+        int quotient = 0;
+        try{
+        quotient = firstNumber / secondNumber; 
+        } catch(ArithmeticException ex){
+        System.out.println(ex.getMessage());
+        } finally {
+        System.out.println(String.format("The answer from division is %s", quotient));   
+        }
+        input.close();
     }
 }
