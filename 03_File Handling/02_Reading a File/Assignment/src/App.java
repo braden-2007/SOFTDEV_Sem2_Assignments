6dc10dfd8dc4fa6f4be6ab1f647fab00448cacc4
Braden Hamilton <hamilbra002@warren.k12.in.us>
2024-02-01 10:56:28 -0500

    bo bo bo bo bo bo bo bo bo bo

--- 03_File Handling/02_Reading a File/Assignment/src/App.java
+++ 03_File Handling/02_Reading a File/Assignment/src/App.java
@@ -1,5 +1,17 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/file.txt";
+
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+       
+        for(var line: lines) {
+            System.out.println(line); 
+         }
     }
 }
