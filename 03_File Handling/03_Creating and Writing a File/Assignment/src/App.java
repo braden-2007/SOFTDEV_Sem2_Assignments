e4df1ed374fd573e3e9105a3b715781a59f33a35
Braden Hamilton <hamilbra002@warren.k12.in.us>
2024-02-07 10:58:09 -0500

    lies

--- 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
+++ 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
@@ -1,5 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/file.txt";
+        
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+       
+        for(var line: lines) {
+            System.out.println(line); 
+        }
+
+
+        FileWriter fileWriter = new FileWriter("src/ender.txt", true);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        
+            
+        for(var line: lines){
+            writer.append(line);
+            writer.append("\n"); 
+        }
+            
+        
+        
+        writer.close();
     }
 }
